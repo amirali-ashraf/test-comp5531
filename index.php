54be13cbc1d976475af0b68f71b01ff578068400
amirali-ashraf <amirali-ashraf.dev@gmail.com>
2021-07-26 22:12:08 -0400

    new p

--- index.php
+++ index.php
@@ -12,5 +12,6 @@
 </head>
 <body>
     <h1>Hello <?= $var?> !</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ratione vero sapiente. Deserunt nesciunt assumenda, tempora perspiciatis at odit voluptatum. Quam labore explicabo voluptates, facere qui consequuntur ut itaque aliquid!</p>
 </body>
 </html>
